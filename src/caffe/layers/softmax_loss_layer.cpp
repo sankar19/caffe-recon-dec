eb7595c6a49720868f9115a7290b826e349ec5b8
sankar19 <sankar19@users.noreply.github.com>
2016-12-06 20:35:43 -0500

    Update softmax_loss_layer.cpp

--- src/caffe/layers/softmax_loss_layer.cpp
+++ src/caffe/layers/softmax_loss_layer.cpp
@@ -33,6 +33,20 @@ void SoftmaxWithLossLayer<Dtype>::LayerSetUp(
   } else {
     normalization_ = this->layer_param_.loss_param().normalization();
   }
+  
+  weight_by_label_freqs_ = this->layer_param_.loss_param().weight_by_label_freqs();
+  
+  if (weight_by_label_freqs_) {
+    vector<int> count_shape(1, this->layer_param_.loss_param().class_weighting_size());
+    label_counts_.Reshape(count_shape);
+    CHECK_EQ(this->layer_param_.loss_param().class_weighting_size(), bottom[0]->channels())
+		<< "Number of class weight values does not match the number of classes.";
+    float* label_count_data = label_counts_.mutable_cpu_data();
+    for (int i = 0; i < this->layer_param_.loss_param().class_weighting_size(); i++) {
+        label_count_data[i] = this->layer_param_.loss_param().class_weighting(i);
+    }
+  }  
+
 }
 
 template <typename Dtype>
@@ -52,6 +66,10 @@ void SoftmaxWithLossLayer<Dtype>::Reshape(
   if (top.size() >= 2) {
     // softmax output
     top[1]->ReshapeLike(*bottom[0]);
+  }
+  if (weight_by_label_freqs_) {
+    CHECK_EQ(this->layer_param_.loss_param().class_weighting_size(), bottom[0]->channels())
+		<< "Number of class weight values does not match the number of classes.";
   }
 }
 
@@ -103,8 +121,14 @@ void SoftmaxWithLossLayer<Dtype>::Forward_cpu(
       }
       DCHECK_GE(label_value, 0);
       DCHECK_LT(label_value, prob_.shape(softmax_axis_));
-      loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],
-                           Dtype(FLT_MIN)));
+      
+      if (weight_by_label_freqs_) {
+        const float* label_count_data = label_counts_.cpu_data();
+        loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],Dtype(FLT_MIN)))
+            * static_cast<Dtype>(label_count_data[label_value]);
+      } else {
+        loss -= log(std::max(prob_data[i * dim + label_value * inner_num_ + j],Dtype(FLT_MIN)));
+      }      
       ++count;
     }
   }
@@ -137,6 +161,11 @@ void SoftmaxWithLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
           }
         } else {
           bottom_diff[i * dim + label_value * inner_num_ + j] -= 1;
+          if (weight_by_label_freqs_) {
+            for (int c = 0; c < bottom[0]->shape(softmax_axis_); ++c) {
+              bottom_diff[i * dim + c * inner_num_ + j] *= static_cast<Dtype>(label_count_data[label_value]);
+            }
+          }          
           ++count;
         }
       }
